eced1aca4d0616cf7e6c2eb770ef929b798e7600
Rakotoarivony Allan-noel <rakotoarivony@a10p14.fil.univ-lille1.fr>
2015-02-23 13:44:44 +0100

    carPlane

---
+++ carPlane_rakotoarivony/src/application/Car.cpp
@@ -0,0 +1,135 @@
+#include <math.h>
+#include "Car.h"
+#include "GLTool.h"
+#include "GLRender.h"
+
+using namespace p3d;
+using namespace std;
+
+
+// ************************************************************
+Car::~Car() {
+}
+
+Car::Car() {
+  _orientation.setIdentity();
+  _position.set(0,0,0);
+  _steering=0;
+  _rotateWheel=0;
+  _acceleration=0;
+  _velocity=0;
+}
+
+// ******************************************************************
+// méthodes à compléter lors du TP
+void Car::drawRim() {
+    float angle = 45;
+    p3d::modelviewMatrix.push();
+    for (int i=0;i<8;i++){
+        p3d::modelviewMatrix.rotate(i*angle,1,0,0);
+        drawCylinderRim();
+    }
+    p3d::modelviewMatrix.pop();
+}
+
+void Car::drawCylinderRim(){
+    p3d::modelviewMatrix.push();
+    p3d::modelviewMatrix.scale(0.1,0.1,2);
+    drawCylinder();
+    p3d::modelviewMatrix.pop();
+}
+
+void Car::drawWheel() {
+    p3d::modelviewMatrix.push();
+    p3d::modelviewMatrix.scale(2.2,2.2,2.2);
+    p3d::diffuseColor=Vector3(0,0,1);
+    drawTorus();
+    p3d::modelviewMatrix.pop();
+
+    p3d::modelviewMatrix.push();
+    p3d::diffuseColor=Vector3(1,0,0);
+    p3d::modelviewMatrix.rotate(90,0,1,0);
+    drawRim();
+    p3d::modelviewMatrix.pop();
+}
+
+void Car::drawAxle() {
+    float length = 6;
+    p3d::modelviewMatrix.push();
+    p3d::diffuseColor=Vector3(0,1,0);
+    p3d::modelviewMatrix.scale(0.2,0.2,length);
+    drawCylinder();
+    p3d::modelviewMatrix.pop();
+    drawWheel();
+    p3d::modelviewMatrix.push();
+    p3d::modelviewMatrix.translate(0,0,length);
+    drawWheel();
+
+    p3d::modelviewMatrix.pop();
+
+}
+
+void Car::drawBody() {
+    p3d::modelviewMatrix.push();
+    p3d::diffuseColor=Vector3(1,0,1);
+    p3d::modelviewMatrix.scale(1,2,1);
+    drawCube();
+    p3d::modelviewMatrix.translate(0,-0.5,2);
+    p3d::modelviewMatrix.scale(1,0.5,2);
+    drawCube();
+    p3d::modelviewMatrix.pop();
+
+}
+
+void Car::draw() {
+    drawAxle();
+}
+
+
+void Car::drawWorld() {
+
+  p3d::modelviewMatrix.push();
+
+  draw(); // tracé de la voiture dans son repère local
+  p3d::modelviewMatrix.pop();
+}
+
+void Car::move() {
+  _acceleration+=-_velocity/50;
+  _velocity+=_acceleration;
+  _rotateWheel+=_velocity*20;
+  _steering-=_steering/10*fabs(_velocity);
+
+  _orientation.rotate(_steering*_velocity/(1.0+fabs(_velocity)),Vector3(0,1,0)); // le /100 est déterminé par essai/erreur
+
+}
+
+
+void Car::accelerate() {
+  _acceleration=0.05;
+
+}
+
+void Car::decelerate() {
+  _acceleration=0;
+}
+
+void Car::brake() {
+  _acceleration=-0.02;
+
+}
+
+void Car::turnLeft() {
+  _steering+=0.5;
+  if (_steering>35) _steering=35;
+}
+
+void Car::turnRight() {
+  _steering-=0.5;
+  if (_steering<-35) _steering=-35;
+}
+
+
+
+
+
